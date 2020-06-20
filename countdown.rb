005d073f21f1d04b465bc24d8ec9763a552a62f7
Ibiene LongJohn <bgirlapollo@gmail.com>
2020-06-20 01:04:15 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
-#write your code here
 
-def countdown
+def countdown(number)
+  while number  > 0
+    #This takes the arguement and puts it the below string
+    puts "#{number} SECOND(S)!"
+    #Subtracts 1 number from th arguement given
+    number -= 1
+  end
+  # Return HAPPY NEW YEAR!
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep(5)
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
