7955a9c40eb259c18abab0998711e0b6543f74e5
Benjamin Anderson <benjaminghray@gmail.com>
2020-06-20 23:25:35 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
-
-def countdown
+x = 1
+while x < 10
+  puts "#{x} is less than 10"
+  x += 1
 end
+def countdown(seconds_to_midnight)
+    while seconds_to_midnight > 0
+      puts "#{seconds_to_midnight} SECOND(S)!"
+      seconds_to_midnight -= 1
+    end
+    "HAPPY NEW YEAR!"
+  end
+  
+  def countdown_with_sleep(seconds_to_midnight)
+    while seconds_to_midnight > 0
+      puts "#{seconds_to_midnight} SECOND(S)!"
+      sleep(1)
+      seconds_to_midnight -= 1
+    end
+    "HAPPY NEW YEAR!"
+  end
